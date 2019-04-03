a569e2127eb5f629f48e5b2b25de961a8e0bde49
Darci <Darci.Burdge@ncc.edu>
2019-04-03 15:24:37 -0400

    Updated Inventory.java with documentation

--- app/src/main/java/edu/ncc/nest/nestapp/Inventory.java
+++ app/src/main/java/edu/ncc/nest/nestapp/Inventory.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 public class Inventory extends AppCompatActivity {
 
     @Override
+    /**
+     * onCreate method -
+     * loads the 'activity_inventory layout' which has only a TextView as a placeholder for now
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
