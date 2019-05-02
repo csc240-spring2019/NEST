194f98e875a0b4639c66440862b7afa260e8367c
Joseph Maltese <josephmaltese012@gmail.com>
2019-05-02 14:21:06 -0400

    Changed launcher activity from MainActivity to Choose, modified Choose

--- app/src/main/java/edu/ncc/nest/nestapp/Choose.java
+++ app/src/main/java/edu/ncc/nest/nestapp/Choose.java
@@ -1,9 +1,11 @@
 package edu.ncc.nest.nestapp;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -30,7 +32,40 @@ public class Choose extends AppCompatActivity {
             case R.id.guestFormBtn:
                 setContentView(R.layout.activity_guest_form);
                 break;
+            case R.id.signUpBtn:
+                createAccountDialog();
+                break;
         }
+    }
+
+    /**
+     * createAccountDialog method - this method creates an alert dialog when the 'Create Nest Account' button is clicked. An alert dialog
+     * will be displayed telling the user what creating an account entails, and will ask them if they want to create an account or not.
+     * If they select 'Yes' they will be directed to the SignUp activity. If they select 'No' the dialog will close.
+     */
+    public void createAccountDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(Choose.this);
+        alertBuilder.setCancelable(false);
+        alertBuilder.setMessage("Creating a NEST account will allow you to receive notifications relating to guest, donator, or volunteer " +
+                "opportunities. You will be allowed to manage your notification preferences once your account is created. Your name, email " +
+                "address, and phone number will be required." + "\n" + "Create an account?");
+        alertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //launch signUp activity
+            }
+        });
+
+        alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = alertBuilder.create();
+
+        alert.show();
     }
 
 }
