e466a3a37cd3793dc4f7a5ae25fc785c40118456
nasjones <nasjonez1@gmail.com>
2019-05-06 12:25:24 -0400

    fixed some itmes in the initial activity

--- app/src/main/java/edu/ncc/nest/nestapp/Choose.java
+++ app/src/main/java/edu/ncc/nest/nestapp/Choose.java
@@ -8,36 +8,53 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.view.View.OnClickListener;
 
-public class Choose extends AppCompatActivity {
+public class Choose extends AppCompatActivity implements OnClickListener {
     private Intent intent;
+    private Button schedule;
+    private Button inventory;
+    private Button guest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
 
-        //this.getWindow().setBackgroundDrawable(null);
+        Button schedule = findViewById(R.id.scheduleBtn);
+        Button inventory = findViewById(R.id.inventoryBtn);
+        Button guest = findViewById(R.id.guestFormBtn);
+
+
+        schedule.setOnClickListener(this);
+        inventory.setOnClickListener(this);
+        guest.setOnClickListener(this);
+
+
+        this.getWindow().setBackgroundDrawableResource(R.drawable.veg_table);
     }
+
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.scheduleBtn:
-                setContentView(R.layout.activity_schedule);
+                intent = new Intent(this, Schedule.class);
+                startActivity(intent);
                 break;
             case R.id.inventoryBtn:
-                setContentView(R.layout.activity_inventory);
+                intent = new Intent(this, Inventory.class);
+                startActivity(intent);
                 break;
             case R.id.guestFormBtn:
-                setContentView(R.layout.activity_guest_form);
-                break;
-            case R.id.volunteerFormBtn:
-                setContentView(R.layout.activity_volunteer_form);
-                intent = new Intent(Choose.this, VolunteerForm.class);
+                intent = new Intent(this, GuestForm.class);
                 startActivity(intent);
                 break;
         }
     }
 
 }
+
+
--- app/src/main/java/edu/ncc/nest/nestapp/MainActivity.java
+++ app/src/main/java/edu/ncc/nest/nestapp/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.ncc.nest.nestapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +18,16 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.forgotPass:
-                //setContentView(R.layout.activity_forgot_password);
+//                intent = new Intent(this, .class);
+//                startActivity(intent);
                 break;
             case R.id.signUp:
-                setContentView(R.layout.activity_sign_up);
+                intent = new Intent(this, SignUp.class);
+                startActivity(intent);
                 break;
             case R.id.log_in_button:
-                setContentView(R.layout.activity_choose);
+                intent = new Intent(this, Choose.class);
+                startActivity(intent);
                 break;
         }
     }
